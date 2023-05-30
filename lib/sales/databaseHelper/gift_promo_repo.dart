9dae2e79fa70afe685a3c8012b0ebed608b6ebae
Ebna_Sina <isina413.is@gmail.com>
2023-05-30 15:21:38 +0600

    Insert and Update tables

--- lib/sales/databaseHelper/gift_promo_repo.dart
+++ lib/sales/databaseHelper/gift_promo_repo.dart
@@ -8,7 +8,7 @@ class GiftPromoRepo {
   DBHelper dbHelper = DBHelper();
 
   ///Promotion Header
-  Future<int> insertIntoPromoHeaderTable(
+  /*Future<int> insertIntoPromoHeaderTable(
       PromoHeaderModel promoHeaderModel) async {
     var dbClient = await conn.db;
     int result = 0;
@@ -20,10 +20,51 @@ class GiftPromoRepo {
       print('There are some issues insertIntoPromoHeaderTable : $e');
     }
     return result;
+  }*/
+
+  Future<int> insertIntoPromoHeaderTable(
+      PromoHeaderModel promoHeaderModel) async {
+    var dbClient = await conn.db;
+    int result = 0;
+    try {
+      var existingRecords = await dbClient!.query(
+        DBHelper.promoHeader,
+        where: 'xtrnnum = ? AND zid = ?',
+        whereArgs: [promoHeaderModel.xtrnnum,
+          promoHeaderModel.zid,],
+      );
+
+      if (existingRecords.isNotEmpty) {
+        result = await dbClient.update(
+          DBHelper.promoHeader,
+          {
+
+            'xstype': promoHeaderModel.xstype,
+            'xfdate': promoHeaderModel.xfdate,
+            'xtdate': promoHeaderModel.xtdate,
+            'xref': promoHeaderModel.xref,
+          },
+          where: 'xtrnnum = ? AND zid = ?',
+          whereArgs: [promoHeaderModel.xtrnnum,
+            promoHeaderModel.zid,],
+        );
+      } else {
+        result = await dbClient.insert(
+          DBHelper.promoHeader,
+          promoHeaderModel.toJson(),
+        );
+      }
+
+      print("-------------$result");
+    } catch (e) {
+      print('There are some issues insertIntoPromoHeaderTable: $e');
+    }
+    return result;
   }
 
+
   /// Promotion details
-  Future<int> insertIntoPromoDetailsTable(
+/*  Future<int> insertIntoPromoDetailsTable(
       PromoDetailsrModel promoDetailsrModel) async {
     var dbClient = await conn.db;
     int result = 0;
@@ -35,10 +76,55 @@ class GiftPromoRepo {
       print('There are some issues insertIntoPromoDetailsTable : $e');
     }
     return result;
+  }*/
+  Future<int> insertIntoPromoDetailsTable(
+      PromoDetailsrModel promoDetailsModel) async {
+    var dbClient = await conn.db;
+    int result = 0;
+    try {
+      var existingRecords = await dbClient!.query(
+        DBHelper.promoDetails,
+        where: 'xtrnnum = ? AND xrow = ?',
+        whereArgs: [
+          promoDetailsModel.xtrnnum,
+          promoDetailsModel.xrow,
+        ],
+      );
+
+      if (existingRecords.isNotEmpty) {
+        result = await dbClient.update(
+          DBHelper.promoDetails,
+          {
+            'xstype': promoDetailsModel.xstype,
+            'xcolor': promoDetailsModel.xcolor,
+            'xtype': promoDetailsModel.xtype,
+            'xfslab': promoDetailsModel.xfslab,
+            'xtslab': promoDetailsModel.xtslab,
+            'xamount': promoDetailsModel.xamount,
+          },
+          where: 'xtrnnum = ? AND xrow = ?',
+          whereArgs: [
+            promoDetailsModel.xtrnnum,
+            promoDetailsModel.xrow,
+          ],
+        );
+      } else {
+        result = await dbClient.insert(
+          DBHelper.promoDetails,
+          promoDetailsModel.toJson(),
+        );
+      }
+
+      print("-------------$result");
+    } catch (e) {
+      print('There are some issues insertIntoPromoDetailsTable: $e');
+    }
+    return result;
   }
 
+
   /// CaCusDisc table
-  Future<int> insertIntoCaCusDiscTable(CaCusDiscModel caCusDiscModel) async {
+/*  Future<int> insertIntoCaCusDiscTable(CaCusDiscModel caCusDiscModel) async {
     var dbClient = await conn.db;
     int result = 0;
     try {
@@ -49,7 +135,49 @@ class GiftPromoRepo {
       print('There are some issues in insertIntoCaCusDiscTable : $e');
     }
     return result;
+  }*/
+  Future<int> insertIntoCaCusDiscTable(CaCusDiscModel caCusDiscModel) async {
+    var dbClient = await conn.db;
+    int result = 0;
+    try {
+      var existingRecords = await dbClient!.query(
+        DBHelper.caCusDisc,
+        where: 'xcus = ? AND xitem = ?',
+        whereArgs: [
+          caCusDiscModel.xcus,
+          caCusDiscModel.xitem,
+        ],
+      );
+
+      if (existingRecords.isNotEmpty)  {
+        result = await dbClient.update(
+          DBHelper.caCusDisc,
+          {
+
+            'xdisc': caCusDiscModel.xdisc,
+            'xdateeff': caCusDiscModel.xdateeff,
+            'xdateexp': caCusDiscModel.xdateexp,
+          },
+          where: 'xcus = ? AND xitem = ?',
+          whereArgs: [
+            caCusDiscModel.xcus,
+            caCusDiscModel.xitem,
+          ],
+        );
+      }else {
+        result = await dbClient.insert(
+          DBHelper.caCusDisc,
+          caCusDiscModel.toJson(),
+        );
+      }
+
+      print("-------------$result");
+    } catch (e) {
+      print('There are some issues in insertIntoCaCusDiscTable: $e');
+    }
+    return result;
   }
+
 
 // Future<void> deleteGiftPromoTable() async {
 //   var dbClient = await conn.db;
